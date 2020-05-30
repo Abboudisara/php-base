80503d7c0ecfb90553e6fbd6d58c083d6c3f172f
Abboudisara <57992532+Abboudisara@users.noreply.github.com>
2020-05-30 17:25:21 +0000

    énoncé 1

---
+++ énoncé1/movies.php
@@ -0,0 +1,29 @@
+<?php
+$Nomfilm=$_GET['Nomfilm'];
+$Nomacteurs=$_GET['Nomacteurs'];
+$annésortie =$_GET['annésortie'];
+
+
+
+echo "<h1>information sur le film $Nomfilm :</h1>" ;
+echo "<p>les acteures de film sont:$Nomacteurs Projection de film dans l année $annésortie  </p>";
+ ?>
+
+
+
+
+
+<!DOCTYPE html>
+ <html lang="en">
+ <head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>énoncé1php</title>
+ </head>
+ <body>
+ 
+  
+ </body>
+ </html>
+
+
